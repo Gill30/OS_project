101a2a1aeb829a873da3cd21019195c4387400f8
Gill30 <gillahmad94@gmail.com>
2016-06-26 18:05:44 +0500

    Using threads
    you can send a message and receive any time.

--- clnt.py
+++ clnt.py
@@ -1,17 +1,59 @@
-#!usr/bin/python3
 
-from socket import *
+#connected=0
+import socket               
+from thread import *
 
-HOST = ''
-PORT = 8086
+#connected_user =[0,0,0,0,0,0,0,0,0,0]
 
-s = socket(AF_INET,SOCK_STREAM)
-s.connect((HOST,PORT))
+s = socket.socket()         
+host = socket.gethostname()
+port = 8887                
+s.connect((host, port))
 
-while True:
-	Message = raw_input('Send:')
-	s.send(Message)
-	Reply = s.recv(1024)
-	print 'Received:', (Reply)
+x=s.recv(29)
 
-s.close()
+print x
+x=s.recv(2)
+
+
+onlin_user=int(x)
+
+
+
+for index in range(onlin_user):
+     x=s.recv(15)
+     print x
+
+
+x=raw_input("enter your name : ")
+s.send(x)
+
+def func(s):
+	while 1:
+		x=s.recv(1024)
+                print x
+               
+
+start_new_thread(func,(s,))
+
+def enter_user(s):
+   other_person=raw_input('Enter no of person to  chat with: ')
+   s.send('!New chat')
+   print '   '
+   s.send(str(other_person))
+ 
+
+
+#enter_user(s)
+
+
+while 1:
+	x=raw_input()
+         
+	if x=='Q' or  x=='q':
+		break
+	elif x=='N' or x=='n':
+                enter_user(s)
+        else :
+		s.send(x)
+s.close()             
